bca7be31283a130ec686a68e98998454bb778753
Thilina Ruwan <thilina20250527@gmail.com>
2025-08-12 18:57:02 +0000

    https://content-provider.payshia.com/tea-jar/factory-fresh-gold-02.webp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,15 +72,15 @@ export default function Home() {
             </p>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-12 w-full max-w-3xl">
                 <div className="flex flex-col items-center gap-4">
-                    <Image src="https://content-provider.payshia.com/tea-jar/ethically-grown.png" alt="Ethically Grown" width={80} height={80} className="object-contain" />
+                    <Image src="https://content-provider.payshia.com/tea-jar/ethically-grown.webp" alt="Ethically Grown" width={80} height={80} className="object-contain" />
                     <h3 className="font-headline text-xl text-neutral-100">Ethically Grown</h3>
                 </div>
                 <div className="flex flex-col items-center gap-4">
-                    <Image src="https://content-provider.payshia.com/tea-jar/hand-picked.png" alt="Hand Picked" width={80} height={80} className="object-contain" />
+                    <Image src="https://content-provider.payshia.com/tea-jar/hand-picked.webp" alt="Hand Picked" width={80} height={80} className="object-contain" />
                     <h3 className="font-headline text-xl text-neutral-100">Hand Picked</h3>
                 </div>
                 <div className="flex flex-col items-center gap-4">
-                    <Image src="https://content-provider.payshia.com/tea-jar/factory-fresh.png" alt="Factory Fresh" width={80} height={80} className="object-contain" />
+                    <Image src="https://content-provider.payshia.com/tea-jar/factory-fresh.webp" alt="Factory Fresh" width={80} height={80} className="object-contain" />
                     <h3 className="font-headline text-xl text-neutral-100">Factory Fresh</h3>
                 </div>
             </div>
