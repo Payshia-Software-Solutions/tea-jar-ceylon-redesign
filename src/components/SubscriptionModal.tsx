dacb96dbdf1221c12b1991874eb32de7747a98ed
Thilina Ruwan <thilina20250527@gmail.com>
2025-08-13 18:03:41 +0000

    http://content-provider.payshia.com/tea-jar/august-banner.webp use hit i

--- src/components/SubscriptionModal.tsx
+++ src/components/SubscriptionModal.tsx
@@ -46,7 +46,7 @@ export function SubscriptionModal() {
                 </div>
             </div>
              <div className="mt-auto">
-                <Image src="https://teajarceylon.com/_next/image?url=%2Fassets%2Fpromo-banners%2Faugust-banner.webp&w=640&q=75" alt="Exceptional Tea Collection" width={250} height={150} className="object-contain"/>
+                <Image src="http://content-provider.payshia.com/tea-jar/august-banner.webp" alt="Exceptional Tea Collection" width={250} height={150} className="object-contain"/>
              </div>
         </div>
 
