62fe0c0277f63646c2e9fcaa9672d3fb980b92c2
Thilina Ruwan <thilina20250527@gmail.com>
2025-08-15 18:12:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -1,9 +1,13 @@
 
 export const pageview = () => {
-    (window as any).fbq('track', 'PageView')
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+        (window as any).fbq('track', 'PageView');
+    }
 }
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (name: string, options = {}) => {
-    (window as any).fbq('track', name, options)
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+        (window as any).fbq('track', name, options);
+    }
 }
